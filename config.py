0dc3ab3f88dad345cd5af34b21fa7ae4ac0543f9
PolinaAlexandr <polinaalexnder@gmail.com>
2019-03-26 23:13:28 +0300

    Inplemented json data generator

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 import time
 
 generator_frequency = 5
+
+RABBIT_HOST= 'localhost'
--- json_generator.py
+++ json_generator.py
@@ -2,18 +2,14 @@ import pika
 import json 
 import time 
 import random
+import config as cfg
+
 from config import generator_frequency
 
-# def send_json():
-#     connection = pika.BaseConnection(pika.ConnectionParameters('localhost'))
-#     channel =  connection.channel()
 
-#     channel.queue_declare(queue='new')
-#     channel.basic_publish(exchange='', routing_key='new', body='New task!')
-#     connection.close()
-    
+
 def generate_item():
-    names = ['Sam', 'Lyn', 'Jack', 'Jonh', 'Lia', 'Margot', 'Flee', 'Victoria', 'Will']
+    names = ['Sam', 'Robert', 'Lyn', 'Jack', 'Jonh', 'Lia', 'Margot', 'Flee', 'Victoria', 'Will']
 
     random_dict = {
         "id" : random.randint(1, 100)*10,
@@ -25,11 +21,21 @@ def generate_item():
     return json.dumps(random_dict)
 
 
+def send_json(item):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(cfg.RABBIT_HOST))
+    channel = connection.channel()
+    channel.queue_declare(queue='people_records')
+    channel.basic_publish(exchange='',
+                      routing_key='hello!',
+                      body=item)
+    print("[x] Sent json data to RabbitMQ", item)
+    connection.close()
+
+
 if __name__ == "__main__":
     while True:
         time.sleep(generator_frequency)
-
-        random_json = generate_item()
-        print(random_json)
+        item = generate_item()
+        send_json(item)
 
 
