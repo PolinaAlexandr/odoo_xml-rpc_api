abe322266d07cb14a34cdc3e19a895371ce1aaed
PolinaAlexandr <polinaalexnder@gmail.com>
2019-03-28 06:19:48 +0300

    Inplemented odoo items creation

--- data_transporter.py
+++ data_transporter.py
@@ -1,8 +1,20 @@
 import pika
 import config as cfg
+import xmlrpc.client
+import json
 
 def callback(ch, method, properties, body):
+    data_dict = json.loads(body)
     print(" [x] Received %r" % body)
+    common = xmlrpc.client.ServerProxy('http://localhost:8069/xmlrpc/2/common')
+    print(common.version())
+    uid = common.authenticate('test_odoo', 'user@domain.com', 'odoo', {})
+
+    # print('uid', uid)
+    
+    models = xmlrpc.client.ServerProxy('http://localhost:8069/xmlrpc/2/object')
+    id = models.execute_kw('test_odoo', uid, 'odoo', 'res.partner', 'create', [data_dict])
+    print(id)
 
 
 if __name__ == "__main__":
